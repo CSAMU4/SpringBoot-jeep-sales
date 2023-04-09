ee093be9a3e051cad1fc66faa47ae297cb2a7781
Candace Samuels <samuelsmcandace@gmail.com>
2023-04-08 23:15:00 -0400

    jeep-sales

---
+++ jeep-sales/src/main/java/com/promineotech/jeep/JeepSales.java
@@ -0,0 +1,15 @@
+package com.promineotech.jeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeepSales {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JeepSales.class, args);
+  }
+ }
+/* Run Spring Boot Application (Expectation: No Errors) then demonstration in browser
+ * Link: http://localhost:8080/swagger-ui/index.html
+ */
